164435b80847470a9b80e7287f473e102329eeda
yhss1015 <135127207+yhss1015@users.noreply.github.com>
2025-03-18 17:23:50 +0900

    [Initial] PlayerSetting

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float maxHp = 100;
+    public float curHp = 100;
+    public float speed = 5;
+    public float attack = 10;
+
+
+
     void Start()
     {
-        Debug.Log("Ω√¿€");
-        Debug.Log("Test1");
-        Debug.Log("PJH pr");
+        
     }
 
     
     void Update()
     {
-        
+        PMove();
+    }
+
+    void PMove()
+    {
+        float speedX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float speedY = Input.GetAxis("Vertical") * (speed / 2) * Time.deltaTime;
+        transform.Translate(speedX, speedY, 0);
     }
 }
