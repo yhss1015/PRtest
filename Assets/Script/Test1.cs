cad605b1e65ce0f63e34b2a7c8dc766553603bfd
JSW0202 <jsu2929@naver.com>
2025-03-18 17:10:33 +0900

    add : test1

---
+++ Assets/Script/Test1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("안녕하세요");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
