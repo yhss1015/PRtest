3001c3f620623ef2f38b0e52f22017212b84f782
yhss1015 <135127207+yhss1015@users.noreply.github.com>
2025-03-19 16:50:45 +0900

    [Add] BasicAttack

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,18 +9,28 @@ public class Player : MonoBehaviour
     public float speedY = 4;
     public float attack = 10;
 
+    public float Level = 1;
+
+    public float BasicAttack_Level = 1;
+
     private Vector3 defaultScale;
     private Animator playerAnim; // 플레이어 애니메이터 가져옴
+
+    public GameObject []attack_Prefab;
 
     void Start()
     {
         defaultScale = transform.localScale; // 초기 스케일 저장
         playerAnim = GetComponent<Animator>();
+
+        // 2초마다 BasicAttackRoutine 실행
+        StartCoroutine(BasicAttackRoutine());
     }
 
     void Update()
     {
         PMove();
+
     }
 
     void PMove()
@@ -52,6 +63,33 @@ public class Player : MonoBehaviour
             playerAnim.SetBool("Idle", true);
         }
     }
+
+    IEnumerator BasicAttackRoutine()
+    {
+        while (true)
+        {
+            BasicAttack();
+            yield return new WaitForSeconds(2f); // 2초 대기 후 반복
+        }
+    }
+
+    void BasicAttack()
+    {
+        if (attack_Prefab.Length < 2) return;
+
+        // 왼쪽 공격
+        Instantiate(attack_Prefab[0], transform.position + attack_Prefab[0].transform.position, attack_Prefab[0].transform.rotation);
+
+        // 0.1초 후 오른쪽 공격 실행
+        StartCoroutine(DelayedAttack());
+    }
+
+    IEnumerator DelayedAttack()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(attack_Prefab[1], transform.position + attack_Prefab[1].transform.position, attack_Prefab[1].transform.rotation);
+    }
+
 
     void TakeDamage(float dmg)
     {
