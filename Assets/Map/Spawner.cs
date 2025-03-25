54806e3079b868e3aa40d7bbb27a92382916b513
YSW <56628542+plzCode@users.noreply.github.com>
2025-03-25 17:52:18 +0900

    Recovery

--- Assets/Map/Spawner.cs
+++ Assets/Map/Spawner.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Diagnostics;
-using System.Linq;
-using Unity.Mathematics;
-using UnityEngine; 
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
@@ -60,8 +58,6 @@ public class Spawner : MonoBehaviour
         
         for (int i = 0; i<monsterNum + (stopwatch.Elapsed.Seconds/10); i++)
         {
-            if (CheckPoolActive() >= monsterMaxNum) continue;
-
             GameObject monster = GameManager.Instance.pool.GetPrefab(monsterLevel);
             if (monster != null)
             {
@@ -87,37 +83,6 @@ public class Spawner : MonoBehaviour
             yield return null; // 한 프레임 대기
         }
         GameManager.Instance.spawner = this;
-    }
-
-    public int CheckPoolActive()
-    {
-        return GameManager.Instance.pool.pools.Sum(pool => pool.Count(obj => obj.activeSelf));
-    }
-
-    public void RandomEvent()
-    {
-        string[] eventFolder = GetEventFolderNames();
-
-        int rand = UnityEngine.Random.Range(0, eventFolder.);
-
-        switch (rand)
-        {
-            case 0: 
-                break;
-        }
-    }
-
-    string[] GetEventFolderNames()
-    {
-        // "Event"라는 이름이 포함된 폴더의 경로를 얻기 위해 Resources 폴더를 탐색
-        string[] allFolders = System.IO.Directory.GetDirectories("Resources/"); // Resources 폴더 내 모든 디렉터리 경로 가져오기
-        // Event로 시작하는 폴더만 선택
-        var eventFolders = allFolders.Where(folder => folder.Contains("Event")).ToArray();
-
-        // 폴더 이름만 추출
-        string[] eventFolderNames = eventFolders.Select(folder => System.IO.Path.GetFileName(folder)).ToArray();
-
-        return eventFolderNames;
     }
 
 }
