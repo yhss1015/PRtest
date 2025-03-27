e0addc6cce7c8e19335f94d281be9546bf7ce77b
박주한 <qkrwngks428@dongguk.edu>
2025-03-27 15:51:21 +0900

    [update] monster

--- Assets/Script/MonsterScripts/Ranger_Monster.cs
+++ Assets/Script/MonsterScripts/Ranger_Monster.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class Ranger_Monster : MonoBehaviour
 {
     public float HP = 20;
     public float Speed = 3;
     public float Attack = 5;
     //public float Dying =1.5f; // 몬스터 사망 모션 시간조절
-    public float KnockBack = 10f; // 피격시 넉백 정도
+    public float KnockBack = 5f; // 피격시 넉백 정도
+    public float Interval = 5f;
+    public float detectionrange = 10f;
+    private float fireTimer;
 
 
     public GameObject target;  //플레이어
     public GameObject EXP;
+    public GameObject Missileprefab;
+    public Transform Shooter;
+    private Transform Player;
+
     Vector2 Dir;
     Vector2 DirNo;
-   
+
     Animator Mob_Ani;
 
     private bool isDead = false; // 몬스터가 죽었는지 확인하는 변수
@@ -36,10 +43,12 @@ public class Monster : MonoBehaviour
 
     void Start()
     {
-        Mob_Ani= GetComponent<Animator>();  
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        Mob_Ani = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-       
+        fireTimer = Interval;
+
         original = sr.color; // 몬스터의 기본 색상 저장 
     }
 
@@ -47,18 +56,44 @@ public class Monster : MonoBehaviour
     {
         if (HP <= 0) return; // 죽었으면 이동하지 않음
 
-        target = GameObject.FindGameObjectWithTag("Player"); 
+        target = GameObject.FindGameObjectWithTag("Player");
 
         Dir = target.transform.position - transform.position;
         DirNo = Dir.normalized;
 
-        transform.Translate(DirNo * Speed *Time.deltaTime);
+        transform.Translate(DirNo * Speed * Time.deltaTime);
 
         if (Dir.x > 0)
             transform.localScale = new Vector3(-1, 1, 1); // 오른쪽
         else if (Dir.x < 0)
             transform.localScale = new Vector3(1, 1, 1); // 왼쪽
 
+        float DistanceToPlayer = Vector2.Distance(transform.position, Player.position);
+
+        if (DistanceToPlayer <= detectionrange)
+        {
+            //플레이어 방향으로 스프라이트 회전
+            sr.flipX = (Player.position.x < transform.position.x);
+
+            //미사일 발사 로직
+            fireTimer -= Time.deltaTime;  // 타이머 감소
+            if (fireTimer < 0)
+            {
+                shoot();  //미사일 발사
+                fireTimer = Interval; //타이머 리셋;
+            }
+        }
+
+    }
+
+    void shoot()
+    {
+        //미사일 생성
+        GameObject missile = Instantiate(Missileprefab, Shooter.position, Quaternion.identity);
+
+        //플레이어 방향으로 발사 방향 설정
+        Vector2 Direction = (Player.position - Shooter.position).normalized;
+        missile.GetComponent<Enemy_Missile>().SetDirection(Direction);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) // 닿을 경우
@@ -78,7 +113,7 @@ public class Monster : MonoBehaviour
     {
         if (collision.CompareTag("Player") && isAttacking) // 태그= Player, isAttacking = true
         {
-            isAttacking = false; 
+            isAttacking = false;
 
             if (DamageCoroutine != null)
             {
@@ -95,13 +130,13 @@ public class Monster : MonoBehaviour
             yield return new WaitForSeconds(DamageTurm); // DamageTrum 마다 피해
         }
 
-        isAttacking=false;
+        isAttacking = false;
         DamageCoroutine = null;
     }
 
     IEnumerator KnockbackCoroutine(Vector2 AttackDir)
     {
-        rb.linearVelocity = AttackDir * KnockBack; 
+        rb.linearVelocity = AttackDir * KnockBack;
         yield return new WaitForSeconds(0.15f); // n초 간 넉백
         rb.linearVelocity = Vector2.zero; // 멈춤
     }
@@ -113,7 +148,7 @@ public class Monster : MonoBehaviour
 
     IEnumerator Flashing()
     {
-        sr.color = new Color(1,1,1,0.5f);
+        sr.color = Color.red;
         yield return new WaitForSeconds(0.1f); // 0.1초 유지  
         sr.color = original; // 색 돌아옴
         Debug.Log("코루틴발생");
@@ -129,11 +164,11 @@ public class Monster : MonoBehaviour
         Vector2 knockbackDir = (transform.position - target.transform.position).normalized; // 넉백 방향
         Knockback(knockbackDir);
 
-        if(HP <= 0)
+        if (HP <= 0)
         {
             isDead = true; // 몬스터 사망 처리
             ExpDrop();
-            Die();  
+            Die();
         }
     }
 
